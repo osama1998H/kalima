f3479a80f0dacb9d2ecc3aaa6dfd2819413d89a5
Abdullah <asalih@creativeadvtech.com>
2024-07-21 15:27:38 +0300

    Exam Students report

---
+++ kalima/kalima/report/department_annual_report/department_annual_report.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, e2next and contributors
+# For license information, please see license.txt
+
+# import frappe
+
+
+def execute(filters=None):
+	columns, data = [], []
+	return columns, data
