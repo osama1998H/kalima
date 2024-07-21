653b3a8356a1ae2237928547abdd0114dcb96d7f
Abdullah <asalih@creativeadvtech.com>
2024-07-21 15:55:20 +0300

    Last Year Students report

--- kalima/kalima/report/department_annual_report/department_annual_report.py
+++ kalima/kalima/report/department_annual_report/department_annual_report.py
@@ -1,8 +1,4 @@
-# Copyright (c) 2024, e2next and contributors
-# For license information, please see license.txt
-
-# import frappe
-
+import frappe
 
 def execute(filters=None):
 	columns, data = [], []
